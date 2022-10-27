7dee37766e51aa233f22f14b30417527e07658c7
10RCA <95744399+10RCA@users.noreply.github.com>
2022-10-27 22:36:58 +0300

    Csharp uygulamaları

---
+++ Uygulama3/Uygulama3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("1. tam sayıyı giriniz");
+            x = Convert.ToInt16(Console.ReadLine());
+            int y;
+            Console.WriteLine("2. tam sayıyı giriniz");
+            y = Convert.ToInt16(Console.ReadLine());
+            if ((x / y) == 0)
+                Console.WriteLine("Girdiğiniz sayılar birbirine tam bölünüyor");
+            else
+                Console.WriteLine("Girdiğiniz sayılar birbirine tam bölünmüyor");
+        }
+    }
+}
